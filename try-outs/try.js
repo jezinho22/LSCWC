e0282cc07c24e97d3632d18a87fd4637d0b10808
Jeremy Johns <jez.johns@gmail.com>
2023-04-19 12:17:11 +0000

    got select working in try.js

--- try-outs/try.js
+++ try-outs/try.js
@@ -1,6 +1,15 @@
-let speakButton = document.querySelector("speak");
-speakButton.addEventListener("click", function () {
-	speechSynthesis.speak(
-		new SpeechSynthesisUtterance("Hello, this is your browser speaking.")
-	);
+// let speakButton = document.querySelector("speak");
+// speakButton.addEventListener("click", function () {
+// 	speechSynthesis.speak(
+// 		new SpeechSynthesisUtterance("Hello, this is your browser speaking.")
+// 	);
+// });
+console.log("js working");
+let clickMe = document.querySelector("#clickMe");
+console.log(clickMe);
+
+clickMe.addEventListener("change", function (event) {
+	console.log("change event fired");
+	console.log(event.target.value);
+	console.log(document.querySelector("#name").value);
 });
