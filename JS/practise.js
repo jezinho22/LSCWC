4e6bb181ecd61d99335a19ca68f08568a51cb953
Jeremy Johns <jez.johns@gmail.com>
2023-04-19 11:16:01 +0000

    forgot to save

--- JS/practise.js
+++ JS/practise.js
@@ -2,9 +2,13 @@ let dropdownList = document.getElementById("dropdownList");
 console.log("practise JS working");
 
 for (let key in wordList) {
-    console.log("working " + key);
-    let opt = document.createElement("option");
-    opt.value = key;
-    opt.textContent = key;
-    dropdownList.appendChild(opt);
+	console.log("working " + key);
+	let opt = document.createElement("option");
+	opt.value = key;
+	opt.textContent = key;
+	dropdownList.appendChild(opt);
 }
+
+function listSelectHandler(event) {}
+
+dropdownList.addEventListener("select", listSelectHandler);
