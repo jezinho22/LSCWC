f21bd8c8d84fdff67d6d626e5826b28dee2a1b3f
Asmaa Sadou <as1304.ammar@gmail.com>
2023-04-20 13:07:47 +0200

    click event for card

--- JS/practise.js
+++ JS/practise.js
@@ -9,11 +9,11 @@ console.log("practise.js link working");
 let dropdownList = document.getElementById("dropdownList");
 // use keys in wordList object in fifty-worst to provide names of lists
 for (let key in wordList) {
-	console.log("working " + key);
-	let opt = document.createElement("option");
-	opt.value = key;
-	opt.textContent = key;
-	dropdownList.appendChild(opt);
+  console.log("working " + key);
+  let opt = document.createElement("option");
+  opt.value = key;
+  opt.textContent = key;
+  dropdownList.appendChild(opt);
 }
 
 // set up event for choosing a list
@@ -21,40 +21,46 @@ dropdownList.addEventListener("change", listSelectHandler);
 
 // uses selected list of words and puts it into an array on its own
 function listSelectHandler(event) {
-	let words = event.target.value;
-	let name = document.getElementById("name").value;
-	// fetch the word list from wordList
-	currentWordList = wordList[words];
-	// create a new turn at the activity
-	myTurn = new Turn(name, words[0]);
-	displayWord();
+  let words = event.target.value;
+  let name = document.getElementById("name").value;
+  // fetch the word list from wordList
+  currentWordList = wordList[words];
+  // create a new turn at the activity
+  myTurn = new Turn(name, words[0]);
+  displayWord();
 }
 function displayWord() {
-	// add word to word box
-	let wordBox = document.getElementById("word");
-	wordBox.textContent = currentWordList[wordIndex][0];
-	// add strategy to strategy box
-	let strategyBox = document.getElementById("strategy");
-	strategyBox.innerHTML = currentWordList[wordIndex][1];
+  // add word to word box
+  let wordBox = document.getElementById("word");
+  wordBox.textContent = currentWordList[wordIndex][0];
+  // add strategy to strategy box
+  let strategyBox = document.getElementById("strategy");
+  strategyBox.innerHTML = currentWordList[wordIndex][1];
 
-	console.log(wordBox.textContent, strategyBox.innerHTML);
+  console.log(wordBox.textContent, strategyBox.innerHTML);
 }
 
 //
 function textSubmit(event) {
-	event.preventDefault();
-	let textbox = event.target.textInput.value;
-	if (textbox === currentWordList[wordIndex][0]) {
-		currentWordList.shift();
-		displayWord();
-	} else {
-		let wrongItem = currentWordList.shift();
-		myTurn.addMisspelled(wrongItem[0]);
-		alert("you are wrong, i will put this to the back, try again in a minute!");
-		currentWordList.push(wrongItem);
-		displayWord();
-	}
+  event.preventDefault();
+  let textbox = event.target.textInput.value;
+  if (textbox === currentWordList[wordIndex][0]) {
+    currentWordList.shift();
+    displayWord();
+  } else {
+    let wrongItem = currentWordList.shift();
+    myTurn.addMisspelled(wrongItem[0]);
+    alert("you are wrong, i will put this to the back, try again in a minute!");
+    currentWordList.push(wrongItem);
+    displayWord();
+  }
 }
 let typeIn = document.getElementById("typeIn");
 
 typeIn.addEventListener("submit", textSubmit);
+
+let flipCard = document.getElementById("cardContainer");
+flipCard.addEventListener("click", flipHandler);
+function flipHandler(event) {
+  flipCard.classList.toggle("card-flip");
+}
