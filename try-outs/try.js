fd171ea44e782127ae728a55e0d4b86c04b11dd8
Jeremy Johns <jez.johns@gmail.com>
2023-04-22 18:02:14 +0000

    made text to speech function in try-outs

--- try-outs/try.js
+++ try-outs/try.js
@@ -1,14 +1,8 @@
-// let speakButton = document.querySelector("speak");
-// speakButton.addEventListener("click", function () {
-// 	speechSynthesis.speak(
-// 		new SpeechSynthesisUtterance("Hello, this is your browser speaking.")
-// 	);
-// });
-
-// toggle class of card container to get flip
-let card = document.getElementById("flip-card");
-card.addEventListener("click", cardFlip);
-
-function cardFlip(event) {
-	card.classList.toggle("flip-me");
+function textToSpeech(message) {
+	utterance = new SpeechSynthesisUtterance(message);
+	voicesAvailable = speechSynthesis.getVoices();
+	// voicesAvailable[0] is make English
+	// [1] and [2] are female English, [13] is Japanese
+	utterance.voice = voicesAvailable[2];
+	speechSynthesis.speak(utterance);
 }
